4905b680283e55325000568284278cd44396ea1d
danglduy <38782010+danglduy@users.noreply.github.com>
2021-06-08 16:33:01 +0700

    Fix animation using custom x position

--- src/App.js
+++ src/App.js
@@ -3,17 +3,44 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 
 function Switch({ status, ...props }) {
-  const className = `switch ${status}`;
+  const containerVariants = {
+    off: {
+      backgroundColor: "#dddddd",
+    },
+    intermediate: {
+      backgroundColor: "#87ebc3",
+    },
+    on: {
+      backgroundColor: "#22cc88",
+    },
+  };
+
+  const itemVariants = {
+    off: {
+      x: "0",
+    },
+    intermediate: {
+      x: "44%",
+    },
+    on: {
+      x: "90%",
+    },
+  };
 
   return (
-    <motion.div animate className={className} {...props}>
-      <motion.div animate />
+    <motion.div
+      animate={containerVariants[status]}
+      className="switch"
+      variants={containerVariants}
+      {...props}
+    >
+      <motion.div animate={itemVariants[status]} variants={itemVariants} />
     </motion.div>
   );
 }
 
 export default function App() {
-  const [status, setStatus] = useState("on");
+  const [status, setStatus] = useState("off");
 
   const handleStatusChange = (event) => {
     const divPosition = event.currentTarget.getBoundingClientRect();
